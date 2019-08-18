72a735d8911669f7888eb162bb09ab942e715d2d
nikkaktus24 <kaktusdima24@gmail.com>
2019-08-18 13:15:38 +0300

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './modules/shared/services/auth-guard/auth.guard';
 import { HomeComponent } from './modules/shared/components/home/home.component';
-import { FeatureFlagGuard } from './modules/shared/services/feature-flag-guard/feature-flag.guard';
 import { RoleGuard } from './modules/shared/services/role-guard/role.guard';
 import { WorkflowTypes } from './modules/shared/enums/workflow-types';
 import { NotFoundComponent } from './modules/shared/components/not-found/not-found.component';
